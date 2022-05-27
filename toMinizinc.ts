08bc1861502bb3be6bb3cdbf5249f505cfd9ba3a
y-lakhdar <ylakhdar@coveo.com>
2022-05-27 16:51:40 -0400

    simplify

--- toMinizinc.ts
+++ toMinizinc.ts
@@ -1,16 +1,42 @@
 import { DocumentBuilder, Metadata, MetadataValue, parseAndGetDocumentBuilderFromJSONDocument } from '@coveo/push-api-client';
+import { PathLike } from 'fs';
 
 type MapValue = string | number | null;
 type MetadataValueMap = Map<string, MapValue[]>;
 
-function encodeString(value: MetadataValue): MapValue {
+export async function getCatalogFieldIds(filePaths: PathLike[]) {
+  const maps = await getMaps(filePaths);
+  const uniqueMetadataKeys: string[][] = [];
+  maps.forEach((map) => {
+    uniqueMetadataKeys.push(findUniqueMetadataKeysInMap(map));
+  });
+  let catalogIdFields = findCatalogIdFields(maps.values(), uniqueMetadataKeys);
+  let objectTypes = Array.from(maps.keys());
+
+  if (catalogIdFields.productIdFields.length === 0) {
+    objectTypes = objectTypes.reverse();
+    catalogIdFields = findCatalogIdFields(maps.values(), uniqueMetadataKeys.reverse());
+  }
+
+  // FIXME: right now we only assume there are 2 object types
+  return {
+    product: {
+      possibleIdFields: catalogIdFields.productIdFields,
+      objectType: objectTypes[0],
+    },
+    variant: {
+      possibleIdFields: catalogIdFields.variantIdFields,
+      objectType: objectTypes[1],
+    },
+  };
+}
+
+function sanitizeMetadataValue(value: MetadataValue): MapValue {
   switch (typeof value) {
     case 'number':
       return value;
-
     case 'string':
       return value;
-
     default:
       return null;
   }
@@ -22,12 +48,12 @@ function addToMap(map: MetadataValueMap, metadata: Metadata) {
     if (mapValue === undefined) {
       mapValue = [];
     }
-    mapValue.push(encodeString(value));
+    mapValue.push(sanitizeMetadataValue(value));
     map.set(key, mapValue);
   }
 }
 
-async function getMaps(filePaths: string[]): Promise<Map<MetadataValue, MetadataValueMap>> {
+async function getMaps(filePaths: PathLike[]): Promise<Map<MetadataValue, MetadataValueMap>> {
   const objectTypeMap: Map<MetadataValue, MetadataValueMap> = new Map();
   const mapBuilderCallback = async (docBuilder: DocumentBuilder) => {
     const { metadata } = docBuilder.build();
@@ -42,7 +68,6 @@ async function getMaps(filePaths: string[]): Promise<Map<MetadataValue, Metadata
     addToMap(metadataValueMap, metadata);
   };
 
-  //   Second parse to build product map
   for (const filePath of filePaths) {
     parseAndGetDocumentBuilderFromJSONDocument(filePath, {
       callback: mapBuilderCallback,
@@ -52,39 +77,7 @@ async function getMaps(filePaths: string[]): Promise<Map<MetadataValue, Metadata
   return objectTypeMap;
 }
 
-export async function getCatalogFieldIds() {
-  const filePaths = [
-    '/Users/ylakhdar/sandbox/dev_hour_10.0/data/raw_catalog/products.json',
-    '/Users/ylakhdar/sandbox/dev_hour_10.0/data/raw_catalog/variants.json',
-  ];
-  // const fieldTable = await getFieldIndexTable(filePaths);
-  const maps = await getMaps(filePaths);
-  const uniqueMetadataKeys: string[][] = [];
-  maps.forEach((map) => {
-    uniqueMetadataKeys.push(findUniqueMetadataKeysInMap(map));
-  });
-  let idFields = findCatalogIdField(maps.values(), uniqueMetadataKeys);
-  let objectTypes = Array.from(maps.keys());
-
-  if (idFields.productIdFields.length === 0) {
-    objectTypes = objectTypes.reverse();
-    idFields = findCatalogIdField(maps.values(), uniqueMetadataKeys.reverse());
-  }
-
-  // FIXME: right now we only assume there are 2 object types
-  return {
-    product: {
-      possibleIdFields: idFields.productIdFields,
-      objectType: objectTypes[0],
-    },
-    variant: {
-      possibleIdFields: idFields.variantIdFields,
-      objectType: objectTypes[1],
-    },
-  };
-}
-
-function findCatalogIdField<T>(maps: IterableIterator<MetadataValueMap>, uniqueMetadataKeys: string[][]) {
+function findCatalogIdFields(maps: IterableIterator<MetadataValueMap>, uniqueMetadataKeys: string[][]) {
   // TODO: can be optimized
   // FIXME: Assuming there is only 2 object types at the moment.
   const [mapA, mapB] = maps;
@@ -104,16 +97,10 @@ function findCatalogIdField<T>(maps: IterableIterator<MetadataValueMap>, uniqueM
   };
 }
 
-function setAreEqual<T>(setA: Set<T>, setB: Set<T>) {
-  if (setA.size !== setB.size) return false;
-  for (const a of setA) if (!setB.has(a)) return false;
-  return true;
-}
-
 function findUniqueMetadataKeysInMap(map: MetadataValueMap): string[] {
   const metadataKeyCandidates: string[] = [];
   for (const [key, values] of map.entries()) {
-    if (!isUniqueAcrossDocument(values)) {
+    if (!isArrayWithDuplicates(values)) {
       continue;
     }
     metadataKeyCandidates.push(key);
@@ -122,22 +109,23 @@ function findUniqueMetadataKeysInMap(map: MetadataValueMap): string[] {
   return metadataKeyCandidates;
 }
 
-function isUniqueAcrossDocument<T>(possibleValues: T[]): boolean {
+// TODO: put the next methods in util files
+
+function setAreEqual<T>(setA: Set<T>, setB: Set<T>) {
+  if (setA.size !== setB.size) {
+    return false;
+  }
+
+  for (const a of setA) {
+    if (!setB.has(a)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function isArrayWithDuplicates<T>(possibleValues: T[]): boolean {
   const set = new Set(possibleValues);
   return set.size === possibleValues.length;
 }
-
-async function main() {
-  console.time('global');
-  const fields = await getCatalogFieldIds();
-  console.timeEnd('global');
-  console.log(fields);
-
-  const mu = process.memoryUsage();
-  // # bytes / KB / MB / GB
-  const gbNow = mu['heapUsed'] / 1024 / 1024;
-  const gbRounded = Math.round(gbNow * 100) / 100;
-
-  console.log(`Heap allocated ${gbRounded} MB`);
-}
-main();
